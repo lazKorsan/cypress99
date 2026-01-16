22cf9cf67fc35800bd1b428d2a60a343ddcf035f
lazKorsan <ahmeteldes2002@gmail.com>
2026-01-16 10:34:49 +0300

    Refactor: Java yapısı silindi, Cypress+Cucumber+Esbuild yapısına geçildi

---
+++ cypress/e2e/StepDefinitions/GoogleSteps.js
@@ -0,0 +1,13 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("Kullanıcı Google anasayfasına gider", () => {
+  cy.visit("https://www.google.com");
+});
+
+When("Arama kutusuna {string} yazar ve enter tuşuna basar", (keyword) => {
+  cy.get('textarea[name="q"], input[name="q"]').type(`${keyword}{enter}`);
+});
+
+Then("Başlığın {string} içerdiğini doğrular", (keyword) => {
+  cy.title().should("include", keyword);
+});
